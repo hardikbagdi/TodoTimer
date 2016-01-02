8bfdf3567054cb58ee9b575b493ccc493f361f1f
Hardik Bagdi <hbagdi1@binghamton.edu>
2016-01-02 00:07:48 -0500

    Settings Toolbar and clean up

--- app/src/main/java/com/bagdi/test/todoTimer/MainActivity.java
+++ app/src/main/java/com/bagdi/test/todoTimer/MainActivity.java
@@ -1,9 +1,7 @@
 package com.bagdi.test.todoTimer;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 implements NavigationView.OnNavigationItemSelectedListener {
@@ -30,7 +29,7 @@ implements NavigationView.OnNavigationItemSelectedListener {
             public void onClick(View view) {
                // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                //         .setAction("Action", null).show();
-               alarmButton(view);
+
             }
         });
 
@@ -101,12 +100,4 @@ implements NavigationView.OnNavigationItemSelectedListener {
         return true;
     }
 
-    public void alarmButton(View view) {
-        Intent intent = new Intent(this, MessageDisplay.class);
-        intent.putExtra(EXTRA_MESSAGE, "HELLO");
-        startActivity(intent);
-
-
-
-    }
 }
--- app/src/main/java/com/bagdi/test/todoTimer/Try1.java
+++ app/src/main/java/com/bagdi/test/todoTimer/Try1.java
@@ -1,15 +1,20 @@
 package com.bagdi.test.todoTimer;
 
-import android.app.ListActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 import com.bagdi.test.todoTimer.db.TaskContract;
@@ -24,17 +29,37 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.LinkedList;
 
-public class Try1 extends ListActivity {
+public class Try1 extends AppCompatActivity {
     private TaskDBHelper helper;
     private ListAdapter listAdapter;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_try1);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //   setSupportActionBar(toolbar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +70,7 @@ public class Try1 extends ListActivity {
             }
         });
 
+
         updateUI();
     }
 
@@ -53,6 +79,10 @@ public class Try1 extends ListActivity {
         super.onResume();
         Log.d("MainActivity", "Onresume for try1 called");
         updateUI();
+        //preference reading
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String v = sharedPreferences.getString("example_text", "-1");
+        Log.d("MainPreference", v);
     }
 
     private void updateUI() {
@@ -70,7 +100,7 @@ public class Try1 extends ListActivity {
 
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String timeString = null;
+        String timeString;
         while (cursor2.moveToNext()) {
             Log.d("MainActivity cursor",
                     cursor2.getString(
@@ -84,7 +114,6 @@ public class Try1 extends ListActivity {
             } else {
                 Log.d("MainActivity cursor", timeString);
             }
-            ;
 
         }
         Cursor cursor = null;
@@ -110,8 +139,8 @@ public class Try1 extends ListActivity {
 //         this.setListAdapter(listAdapter);
         final CountdownAdapter countdownAdapter = new CountdownAdapter(getApplicationContext(), new LinkedList<Task>(Arrays.asList(tasks)),this);
         final ArrayList<Task> TaskList = new ArrayList<Task>(Arrays.asList(tasks));
-
-        this.setListAdapter(countdownAdapter);
+        ListView listView = (ListView) findViewById(R.id.TaskListView);
+        listView.setAdapter(countdownAdapter);
     }
 
 
